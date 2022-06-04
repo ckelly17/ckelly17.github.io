bac1b2dfeeb5f15596942921332be0c1587dcaab
ckelly17 <conor.richard.kelly@gmail.com>
2022-06-03 21:06:37 -0400

    render

--- render_mass.R
+++ render_mass.R
@@ -14,13 +14,3 @@ graphics.off()
 # render
 rmarkdown::render("app/mass.Rmd",
                   envir = globalenv()) # make sure global env is referenced
-
-system("git remote add tiger https://github.com/ckelly17/ckelly17.github.io.git")
-system("git status")
-system("git add 'app/mass.html' ")
-system("git commit -m 'updating wastewater tracker' ")
-system("git push")
-
-
-
-
