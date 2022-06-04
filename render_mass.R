57b3698b5a3a189073ff1399b080f5f72a84033d
ckelly17 <conor.richard.kelly@gmail.com>
2022-06-03 21:04:04 -0400

    remove setwd wastewater

--- render_mass.R
+++ render_mass.R
@@ -15,12 +15,6 @@ graphics.off()
 rmarkdown::render("app/mass.Rmd",
                   envir = globalenv()) # make sure global env is referenced
 
-file.copy("app/mass.html", "output/mass.html")
-
-
-setwd("/Users/conorkelly/Documents/Heroku Apps/covid_Rmd/output")
-
-
 system("git remote add tiger https://github.com/ckelly17/ckelly17.github.io.git")
 system("git status")
 system("git add 'app/mass.html' ")
