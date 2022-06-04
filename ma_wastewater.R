e304449c3ba3a820b82646cd7ba92b02d6024df3
ckelly17 <conor.richard.kelly@gmail.com>
2022-06-03 22:32:43 -0400

    xml2 v2

--- ma_wastewater.R
+++ ma_wastewater.R
@@ -5,6 +5,8 @@ if("pacman" %in% installed.packages()){
   library(pacman)
 }
 
+p_load(devtools)
+devtools::install_github("r-lib/xml2")
 system("sudo apt install libcurl4-openssl-dev libssl-dev libxml2-dev")
 print("command went through")
 
