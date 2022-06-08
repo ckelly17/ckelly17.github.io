e85f86b55bc97c6a6c7dd58e545b0d5741012946
ckelly17 <conor.richard.kelly@gmail.com>
2022-06-08 14:42:42 -0400

    adding google trends analysis

---
+++ scripts/google_trends_covid/google_trends_covid.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import pytrends
+from pytrends.request import TrendReq
+import plotly.express as px
+import plotly.graph_objects as go
+
+pytrends = TrendReq(hl='en-US', tz=360)
+kw_list = ["covid test"]
+pytrends.build_payload(kw_list = kw_list, timeframe='today 5-y', geo='US')
+
+hist = pytrends.interest_over_time().reset_index()
+hist = hist[hist['date'] >= '2020-01-01']
+hist.tail()
+
+fig_line = px.line(hist, x='date', y="covid test")
+
+fig_line['data'][0]['line']['color']='rgb(94,25,20)'
+fig_line['data'][0]['line']['width']=3
+
+title = '<b>Google Search Interest for </b><i>"covid test"</i><b> Over Time</b>'
+
+fig_line.update_layout(
+    
+     title={
+        'text': title,
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    
+    font_family="Arial",
+    font_color="black",
+    title_font_family="Arial",
+    title_font_color="black",
+    legend_title_font_color="black",
+
+    #paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+
+fig_line.update_yaxes(title ="", linewidth=2, linecolor='black', gridcolor='rgb(220,220,220)')
+fig_line.update_xaxes(title ="", linewidth=2, linecolor='black', gridcolor='rgb(220,220,220)')
+
+by_state = pytrends.interest_by_region(resolution='COUNTRY', inc_low_vol=True, inc_geo_code=True).reset_index()
+by_state['country'] = 'US'
+by_state['state_code'] = by_state['geoCode'].str.replace('US-', '').str.strip()
+
+fig = go.Figure(data=go.Choropleth(
+                    locations=by_state['state_code'],
+                    z=by_state['covid test'].astype(float),
+                    locationmode='USA-states',
+                    #colorscale='Reds',
+                    colorscale=[[0, 'rgb(205,92,92)'], [1, 'rgb(94,25,20)']],
+                    autocolorscale=False,
+                    text=by_state['geoName'], # hover text
+                    marker_line_color='white', # line markers between states
+                    colorbar_title="",
+                    
+))
+
+title = '<b>Google Search Interest for </b><i>"covid test"</i><b> by State</b>'
+
+fig.update_layout(
+    title={
+        'text': title,
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    geo = dict(
+        scope='usa',
+        projection=go.layout.geo.Projection(type = 'albers usa'),
+        showlakes=True, # lakes
+        lakecolor='rgb(255, 255, 255)'),
+    
+    font_family="Arial",
+    font_color="black",
+    title_font_family="Arial",
+    title_font_color="black",
+    legend_title_font_color="black",
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+
+
+with open('google_trends_covid.html', 'a') as f:
+    f.write(fig_line.to_html(full_html=False, include_plotlyjs='cdn'))
+    f.write(fig.to_html(full_html=False, include_plotlyjs='cdn'))
